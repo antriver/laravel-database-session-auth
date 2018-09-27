5ec725a55a3162cdaf5d5f0ece2c5cfb215ee984
Anthony Kuske <anthonykuske@gmail.com>
2018-09-27 21:59:06 +0100

    Initial commit

---
+++ src/DatabaseSessionAuthServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tmd\LaravelDatabaseSessionAuth;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseSessionAuthServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Auth::extend(
+            'database-session',
+            function (Container $app, $name, array $config) {
+                return new DatabaseSessionGuard(
+                    app('auth')->createUserProvider($config['provider']),
+                    $app->make(Request::class)
+                );
+            }
+        );
+    }
+
+}
